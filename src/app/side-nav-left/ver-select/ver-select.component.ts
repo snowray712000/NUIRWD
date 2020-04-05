ba4d2de290cdb2e1fc5d04436b077229a9763649
Emmanuel.Lo <snowray712000@gmail.com>
2020-04-05 18:51:53 +0800

    版本 20200405

--- src/app/side-nav-left/ver-select/ver-select.component.ts
+++ src/app/side-nav-left/ver-select/ver-select.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
-
+import { BibleVersionQueryService } from '../../fhl-api/bible-version-query.service';
+import { of } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+import { IBibleVersionQueryService } from 'src/app/fhl-api/IBibleVersionQueryService';
 @Component({
   selector: 'app-ver-select',
   templateUrl: './ver-select.component.html',
@@ -8,7 +11,7 @@ import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 export class VerSelectComponent implements OnInit {
   private versionQ: IVersionsQuery;
   @ViewChildren('verC', { read: false }) vers: QueryList<any>;
-  verClass = [
+  verClass = of([
     {
       na: '中文',
       vers: [
@@ -23,13 +26,18 @@ export class VerSelectComponent implements OnInit {
         { na: '新譯本1', eng: 'cnv1' }
       ],
     },
-  ];
-  constructor() { }
+  ]);
+  constructor() {
+    this.versionQ = new VersionsQuery(undefined);
+  }
 
   ngOnInit() {
     if (this.versionQ !== undefined) {
-      this.verClass = this.versionQ.queryBibleVersion();
+      this.verClass = this.versionQ.queryBibleVersionAsync();
     }
+
+    let aa = new BibleVersionQueryService();
+    aa.queryBibleVersionsAsync().toPromise().then(a1 => console.log(a1));
   }
   onSelectChanged() {
     console.log(this.getVersionsFromVerCControls());
@@ -60,5 +68,70 @@ export class VerSelectComponent implements OnInit {
 }
 
 interface IVersionsQuery {
-  queryBibleVersion();
+  queryBibleVersionAsync();
+}
+
+class VersionsQuery implements IVersionsQuery {
+  private static subClass = [
+    { na: '中文', engs: 'unv,ncv,tcv95,recover,lcc,wlunv,ddv,csb,cnet,cccbst,nt1864,mor1823' },
+    { na: '原文', engs: 'cbol,bhs,fhlwh,lxx,esv' },
+    { na: '英文', engs: 'kjv,bbe,web,asv,darby,erv' },
+    { na: '民族方言', engs: 'apskcl,tte,apskhl,bklcl,bklhl,prebklcl,prebklhl,thv2e,hakka,sgebklcl,sgebklhl,rukai,tsou,ams,ttnt94' },
+    { na: '尚未分類', engs: '' }
+  ];
+  private bibleQ: IBibleVersionQueryService;
+  constructor(bibleQ: IBibleVersionQueryService) {
+    this.bibleQ = bibleQ;
+    if (this.bibleQ === undefined) {
+      this.bibleQ = new BibleVersionQueryService();
+    }
+  }
+  queryBibleVersionAsync() {
+    return this.bibleQ.queryBibleVersionsAsync().pipe(
+      //tap(a1 => console.log(a1),
+      map(a1 => {
+        const r1 = {};
+        VersionsQuery.subClass.forEach(a2 => r1[a2.na] = []);
+        // console.log(r1);
+
+        const r2 = {};
+        VersionsQuery.subClass.forEach(a2 => r2[a2.na] = new RegExp(this.gRegexString(a2.engs)));
+        // console.log(r2);
+
+        a1.forEach(a2 => {
+          let isFinded = false;
+          for (const key in r2) {
+            if (r2.hasOwnProperty(key)) {
+              const reg = r2[key];
+              if (a2.na.match(reg)) {
+                // console.log(a2);
+                r1[key].push({ na: a2.naChinese, eng: a2.na });
+                isFinded = true;
+                break;
+              }
+            }
+          }
+          if (isFinded === false) {
+            r1['尚未分類'].push({ na: a2.naChinese, eng: a2.na });
+          }
+        });
+        // console.log(r1);
+
+        const re = [];
+        for (const key in r1) {
+          if (r1.hasOwnProperty(key)) {
+            const element = r1[key];
+            re.push({ na: key, vers: element });
+          }
+        }
+        console.log(re);
+
+        return re;
+      }),
+    );
+  }
+  /** cbol,bhs,fhlwh,lxx */
+  private gRegexString(engs: string) {
+    return '^(' + engs.split(',').join(')|(') + ')$';
+  }
 }
